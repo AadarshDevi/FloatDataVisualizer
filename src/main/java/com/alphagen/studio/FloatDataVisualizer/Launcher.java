859197ebea5cdef43227b5707ea55789db21a7c3
Aadarsh Devi <aadarsh123456char@gmail.com>
2025-11-22 16:36:40 -0800

    added interrupt and sleep for killData keeper and receiver. added getters for keeper and receiver.

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/Launcher.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/Launcher.java
@@ -1,7 +1,7 @@
 package com.alphagen.studio.FloatDataVisualizer;
 
+import com.alphagen.studio.FloatDataVisualizer.data.DataConfigurator;
 import com.alphagen.studio.FloatDataVisualizer.data.DataKeeper;
-import com.alphagen.studio.FloatDataVisualizer.data.Settings;
 import com.alphagen.studio.FloatDataVisualizer.datareciever.DataReceiver;
 import javafx.application.Application;
 
@@ -15,28 +15,33 @@ public class Launcher {
 
     public static void main(String[] args) {
 
-        Settings setting = Settings.getInstance();
+        DataConfigurator setting = DataConfigurator.getInstance();
         setting.readSettings();
 
-            DataKeeper dataKeeper = DataKeeper.getInstance();
-            dkt = new Thread(dataKeeper);
-            dkt.setName("DataKeeper");
-            dkt.start();
+        DataKeeper dataKeeper = DataKeeper.getInstance();
+        dkt = new Thread(dataKeeper);
+        dkt.setName("DataKeeper");
+        dkt.start();
 
-            DataReceiver dataReceiver = DataReceiver.getInstance();
-            drt = new Thread(dataReceiver);
-            drt.setName("DataReceiver");
-            drt.start();
+        DataReceiver dataReceiver = DataReceiver.getInstance();
+        drt = new Thread(dataReceiver);
+        drt.setName("DataReceiver");
+        drt.start();
 
         Application.launch(Main.class, args);
     }
 
     public static void killDataReceiver() {
+        drt.interrupt();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         if (drt.isAlive()) {
             JOptionPane.showMessageDialog(null, "Unable to kill DataReceiver", "Launcher Exception", JOptionPane.ERROR_MESSAGE);
             System.err.println("ERROR: Data Receiver is still running");
-        }
-        else System.out.println("LOG: Closed > Data Receiver");
+        } else System.out.println("LOG: Closed > Data Receiver");
     }
 
     public static boolean isDataReceiverAlive() {
@@ -44,12 +49,25 @@ public class Launcher {
     }
 
     public static void killDataKeeper() {
-        DataKeeper.getInstance().stop();
+//        DataKeeper.getInstance().stop();
+        dkt.interrupt();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         if (dkt.isAlive()) {
             JOptionPane.showMessageDialog(null, "Unable to kill DataKeeper", "Launcher Exception", JOptionPane.ERROR_MESSAGE);
             System.err.println("ERROR: Data Keeper is still running");
-        }
-        else System.out.println("LOG: Closed > Data Keeper");
+        } else System.out.println("LOG: Closed > Data Keeper");
+    }
+
+    public static Thread getDataKeeperThread() {
+        return dkt;
+    }
+
+    public static Thread getDataReceiverThread() {
+        return drt;
     }
 
     // Platform Obj in Settings
