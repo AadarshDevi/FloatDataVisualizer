58695c95fd722d4364cdc24b873a077d92301d5c
Aadarsh Devi <aadarsh123456char@gmail.com>
2025-11-22 16:31:43 -0800

    DataReader in DataPlotter in Main uses interrupt.

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/Main.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/Main.java
@@ -1,6 +1,6 @@
 package com.alphagen.studio.FloatDataVisualizer;
 
-import com.alphagen.studio.FloatDataVisualizer.data.Settings;
+import com.alphagen.studio.FloatDataVisualizer.data.DataConfigurator;
 import com.alphagen.studio.FloatDataVisualizer.data.DataKeeper;
 import com.alphagen.studio.FloatDataVisualizer.data.DataPointRecord;
 import com.alphagen.studio.FloatDataVisualizer.datarecorder.DataPlotter;
@@ -20,44 +20,44 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/float_data_recorder.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/float_data_recorder.fxml"));
 
-            BorderPane floatUI = fxmlLoader.load();
-            DataPlotter dataPlotter = fxmlLoader.getController();
+        BorderPane floatUI = fxmlLoader.load();
+        DataPlotter dataPlotter = fxmlLoader.getController();
 
-            if (dataPlotter == null) {
-                JOptionPane.showMessageDialog(null, "DataPlotter is null.", "Main Exception", JOptionPane.ERROR_MESSAGE);
-                System.err.println("ERROR: DataPlotter is Null");
-                System.exit(0);
+        if (dataPlotter == null) {
+            JOptionPane.showMessageDialog(null, "DataPlotter is null.", "Main Exception", JOptionPane.ERROR_MESSAGE);
+            System.err.println("ERROR: DataPlotter is Null");
+            System.exit(0);
+        }
+
+        Scene scene = new Scene(floatUI);
+        stage.setTitle("Miramar Water Jets Float Data Visualizer " + DataConfigurator.getInstance().getReleaseVersion());
+        stage.setScene(scene);
+        Image icon = new Image(Main.class.getResourceAsStream("float_data_recorder_2_png_icon_2.png"));
+        stage.getIcons().add(icon);
+        stage.setOnCloseRequest(event -> {
+            if (!Launcher.isDataReceiverAlive()) {
+                Launcher.killDataReceiver();
+                Launcher.killDataKeeper();
             }
+            System.out.println("LOG: Closing DataRecorder");
+            System.exit(0);
+        });
+        stage.show();
 
-            Scene scene = new Scene(floatUI);
-            stage.setTitle("Miramar Water Jets Float Data Visualizer " + Settings.getInstance().getReleaseVersion());
-            stage.setScene(scene);
-            Image icon = new Image(Main.class.getResourceAsStream("float_data_recorder_2_png_icon_2.png"));
-            stage.getIcons().add(icon);
-            stage.setOnCloseRequest(event -> {
-                if (!Launcher.isDataReceiverAlive()) {
-                    Launcher.killDataReceiver();
-                    Launcher.killDataKeeper();
+        Thread dataReader = new Thread(() -> {
+//                    boolean running = true;
+//            int nullValues = 0;
+            while (!Thread.currentThread().isInterrupted()) {
+                DataPointRecord dataPointRecord = DataKeeper.getInstance().getDataPointRecord();
+                if (dataPointRecord != null) {
+                    Platform.runLater(() -> dataPlotter.writeDataPoint(dataPointRecord));
                 }
-                System.out.println("LOG: Closing DataRecorder");
-                System.exit(0);
-            });
-            stage.show();
-
-                Thread dataReader = new Thread(() -> {
-                    boolean running = true;
-                    int nullValues = 0;
-                    while (running) {
-                        DataPointRecord dataPointRecord = DataKeeper.getInstance().getDataPointRecord();
-                        if (dataPointRecord != null) {
-                            Platform.runLater(() -> dataPlotter.writeDataPoint(dataPointRecord));
-                        }
-                    }
-                });
-                dataReader.setName("DataReader");
-                dataReader.setDaemon(true);
-                dataReader.start();
+            }
+        });
+        dataReader.setName("DataReader");
+        dataReader.setDaemon(true);
+        dataReader.start();
     }
 }
