42154cae0d0f80981605cc83d949115cc784bfa5
Aadarsh Devi <aadarsh123456char@gmail.com>
2025-11-29 11:15:55 -0800

    Added all packages to exports.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,12 @@ module com.alphagen.studio.FloatDataVisualizer {
     opens com.alphagen.studio.FloatDataVisualizer.datarecorder to javafx.fxml;
     opens com.alphagen.studio.FloatDataVisualizer.datareciever to com.fazecast.jSerialComm;
     exports com.alphagen.studio.FloatDataVisualizer;
-    exports com.alphagen.studio.FloatDataVisualizer.datarecorder;
     exports com.alphagen.studio.FloatDataVisualizer.data;
+    exports com.alphagen.studio.FloatDataVisualizer.data.util;
+    exports com.alphagen.studio.FloatDataVisualizer.datareciever;
+    exports com.alphagen.studio.FloatDataVisualizer.datarecorder;
+    exports com.alphagen.studio.FloatDataVisualizer.datawriter;
+    exports com.alphagen.studio.FloatDataVisualizer.filepaths;
+    exports com.alphagen.studio.FloatDataVisualizer.log;
+    opens com.alphagen.studio.FloatDataVisualizer to javafx.controls;
 }
