98d7009b95910ba2ba9533b428da6eddc560ed57
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-06 18:33:35 -0800

    updated maven files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,20 +1,24 @@
 module com.alphagen.studio.FloatDataVisualizer {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.fazecast.jSerialComm;
-    requires javafx.graphics;
-    requires java.desktop;
-    requires javafx.swing;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires com.fazecast.jSerialComm;
+	requires javafx.graphics;
+	requires java.desktop;
+	requires javafx.swing;
+	requires static lombok;
 
-    opens com.alphagen.studio.FloatDataVisualizer.datarecorder to javafx.fxml;
-    opens com.alphagen.studio.FloatDataVisualizer.datareciever to com.fazecast.jSerialComm;
-    exports com.alphagen.studio.FloatDataVisualizer;
-    exports com.alphagen.studio.FloatDataVisualizer.data;
-    exports com.alphagen.studio.FloatDataVisualizer.data.util;
-    exports com.alphagen.studio.FloatDataVisualizer.datareciever;
-    exports com.alphagen.studio.FloatDataVisualizer.datarecorder;
-    exports com.alphagen.studio.FloatDataVisualizer.datawriter;
-    exports com.alphagen.studio.FloatDataVisualizer.filepaths;
-    exports com.alphagen.studio.FloatDataVisualizer.log;
-    opens com.alphagen.studio.FloatDataVisualizer to javafx.controls;
+	opens com.alphagen.studio.FloatDataVisualizer to javafx.controls, javafx.fxml;
+	opens com.alphagen.studio.FloatDataVisualizer.buoyui.controllers to javafx.controls, javafx.fxml;
+//    opens com.alphagen.studio.FloatDataVisualizer.datarecorder to javafx.fxml;
+//    opens com.alphagen.studio.FloatDataVisualizer.datareciever to com.fazecast.jSerialComm;
+
+	exports com.alphagen.studio.FloatDataVisualizer;
+	exports com.alphagen.studio.FloatDataVisualizer.buoyui.controllers;
+//    exports com.alphagen.studio.FloatDataVisualizer.data;
+//    exports com.alphagen.studio.FloatDataVisualizer.data.util;
+//    exports com.alphagen.studio.FloatDataVisualizer.datareciever;
+//    exports com.alphagen.studio.FloatDataVisualizer.datarecorder;
+//    exports com.alphagen.studio.FloatDataVisualizer.datawriter;
+//    exports com.alphagen.studio.FloatDataVisualizer.filepaths;
+//    exports com.alphagen.studio.FloatDataVisualizer.log;
 }
