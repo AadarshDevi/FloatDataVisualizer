3b655e2e69483b154ce33fc0c10b74ec2f8e31c6
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-07 19:26:32 -0800

    updated correct connection editor fxml file

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/connections/ConnectionUIConstants.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/connections/ConnectionUIConstants.java
@@ -3,5 +3,5 @@ package com.alphagen.studio.FloatDataVisualizer.buoyui.connections;
 public class ConnectionUIConstants {
 	public static final String CONNECTIONS_PAGE = "buoyui/connections/Connections_v2.fxml";
 	public static final String DATA_CARD = "DataCard_v2_3.fxml";
-	public static final String CONNECTION_EDITOR = "ce3.fxml";
+	public static final String CONNECTION_EDITOR = "ConnectionsEditor_v3.fxml";
 }
