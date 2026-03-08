f1eb9a78baaaa269f3a7258ee977160ed79128e5
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-07 19:01:27 -0800

    stores the different stages

---
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/managers/StageManager.java
@@ -0,0 +1,16 @@
+package com.alphagen.studio.FloatDataVisualizer.buoyui.managers;
+
+import javafx.stage.Stage;
+import lombok.Getter;
+import lombok.Setter;
+
+public class StageManager {
+
+	@Getter
+	@Setter
+	private static Stage mainStage;
+
+	@Getter
+	@Setter
+	private static Stage connectionCreatorStage;
+}
