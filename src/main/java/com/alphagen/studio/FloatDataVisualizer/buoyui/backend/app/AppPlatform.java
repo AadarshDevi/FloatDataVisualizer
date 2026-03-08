e941f90f226fafdf1e57bed01f47ac843f4e4397
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-07 22:01:50 -0800

    app platforms

---
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/app/AppPlatform.java
@@ -0,0 +1,12 @@
+package com.alphagen.studio.FloatDataVisualizer.buoyui.backend.app;
+
+public enum AppPlatform {
+	Win11_X86, // 100%
+	Win11_ARM, // No Device
+
+	MACOS_INTEL, // Apple Dev Account Needed
+	MACOS_ARM, // Apple Dev Account Needed
+
+	LINUX_X86, // Device Needed
+	LINUX_ARM // Device Needed
+}
