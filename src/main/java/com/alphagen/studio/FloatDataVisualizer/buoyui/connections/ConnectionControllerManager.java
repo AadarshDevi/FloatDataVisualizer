e1af198ce585c2aea1a8eae0060f72e4fa0225cd
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-07 18:56:58 -0800

    stores connection controller to be accessed b different places

---
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/connections/ConnectionControllerManager.java
@@ -0,0 +1,11 @@
+package com.alphagen.studio.FloatDataVisualizer.buoyui.connections;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class ConnectionControllerManager {
+
+	@Setter
+	@Getter
+	private static ConnectionsController connectionsController;
+}
