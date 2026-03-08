63c01dc8863d701569f1580d40c0317ff7e66e12
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-07 19:00:43 -0800

    list of ui files for easy access

---
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/connections/ConnectionUIConstants.java
@@ -0,0 +1,7 @@
+package com.alphagen.studio.FloatDataVisualizer.buoyui.connections;
+
+public class ConnectionUIConstants {
+	public static final String CONNECTIONS_PAGE = "buoyui/connections/Connections_v2.fxml";
+	public static final String DATA_CARD = "DataCard_v2_3.fxml";
+	public static final String CONNECTION_EDITOR = "ce3.fxml";
+}
