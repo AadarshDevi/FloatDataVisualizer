5371c1430d0f3badb4c6995510d323c84d0a4a9f
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-07 19:00:55 -0800

    class files

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/controllers/ConnectionType.java
+++
@@ -1,6 +0,0 @@
-package com.alphagen.studio.FloatDataVisualizer.buoyui.controllers;
-
-public enum ConnectionType {
-	SERIAL,
-	WIRELESS
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,13 +8,13 @@ module com.alphagen.studio.FloatDataVisualizer {
 	requires static lombok;
 
 	opens com.alphagen.studio.FloatDataVisualizer to javafx.controls, javafx.fxml;
-	opens com.alphagen.studio.FloatDataVisualizer.buoyui.controllers to javafx.controls, javafx.fxml;
+	opens com.alphagen.studio.FloatDataVisualizer.buoyui.connections to javafx.controls, javafx.fxml;
 //    opens com.alphagen.studio.FloatDataVisualizer.datarecorder to javafx.fxml;
 //    opens com.alphagen.studio.FloatDataVisualizer.datareciever to com.fazecast.jSerialComm;
 
 	exports com.alphagen.studio.FloatDataVisualizer;
-	exports com.alphagen.studio.FloatDataVisualizer.buoyui.controllers;
-//    exports com.alphagen.studio.FloatDataVisualizer.data;
+	exports com.alphagen.studio.FloatDataVisualizer.buoyui.connections;
+	//    exports com.alphagen.studio.FloatDataVisualizer.data;
 //    exports com.alphagen.studio.FloatDataVisualizer.data.util;
 //    exports com.alphagen.studio.FloatDataVisualizer.datareciever;
 //    exports com.alphagen.studio.FloatDataVisualizer.datarecorder;
