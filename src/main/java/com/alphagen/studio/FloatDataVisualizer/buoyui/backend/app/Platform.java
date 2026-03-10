8e56e3624dbceb7fa8e26a12442c9f06dc23576c
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-10 09:31:50 -0700

    moved to backend

---
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/app/Platform.java
@@ -0,0 +1,9 @@
+package com.alphagen.studio.FloatDataVisualizer.buoyui.backend.app;
+
+public enum Platform {
+    WIN11, // 100%
+    MACOS, // Apple Dev Account Needed
+    DEBIAN_LINUX,
+    REDHAT_LINUX,
+    NULL, LINUX // Device Needed
+}
