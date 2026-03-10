03fc8b87a84b87096c6b8a90f0da58545127c4d4
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-10 19:24:34 +0000

    removed end app line;)

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/Launcher.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/Launcher.java
@@ -93,7 +93,6 @@ public class Launcher extends Application {
 
         PlatformDetector pd = new PlatformDetector();
         pd.detectPlatform();
-        System.exit(0);
 
         System.out.println("Initializing Backend");
         Backend backend = Backend.getBackend();
