b577b908ab644b39fd8bdf9ee49c9fc5092e38de
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-10 07:03:56 -0700

    list of folders

---
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/constants/FolderConstants.java
@@ -0,0 +1,6 @@
+package com.alphagen.studio.FloatDataVisualizer.buoyui.backend.constants;
+
+public class FolderConstants {
+	public static final String CONNECTIONS = "connections";
+	public static final String LOGS = "logs";
+}
