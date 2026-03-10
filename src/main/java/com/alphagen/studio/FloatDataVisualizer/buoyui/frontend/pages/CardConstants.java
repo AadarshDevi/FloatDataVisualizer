a9fbaf32e03f88c370bd636743b1851dea8f9a7c
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-10 06:58:14 -0700

    constants for fxml files

---
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/pages/CardConstants.java
@@ -0,0 +1,8 @@
+package com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.pages;
+
+import java.net.URL;
+
+public class CardConstants {
+	public static final String FRONTEND_ROOT = PageConstants.FRONTEND_ROOT;
+	public static final URL CONNECTION_DATA_CARD = PageConstants.class.getResource(FRONTEND_ROOT + "/pages/connections/datacard/DataCard_v2_3.fxml");
+}
---
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/pages/PageConstants.java
@@ -0,0 +1,10 @@
+package com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.pages;
+
+import java.net.URL;
+
+public class PageConstants {
+	public static final String FRONTEND_ROOT = "/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend";
+	public static final URL CONNECTIONS_PAGE = PageConstants.class.getResource(FRONTEND_ROOT + "/pages/connections/Connections_v2.fxml");
+	public static final URL CONNECTIONS_EDITOR_PAGE = PageConstants.class.getResource(FRONTEND_ROOT + "/pages/connections/editor/ConnectionsEditor_v4.fxml");
+	public static final URL LOADING_SCREEN = PageConstants.class.getResource(FRONTEND_ROOT + "/util/loading/ls_1.fxml");
+}
