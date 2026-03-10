3e076e8b1680b64791b3f6c850032426d383f919
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-10 07:04:33 -0700

    correct open and exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,24 +7,34 @@ module com.alphagen.studio.FloatDataVisualizer {
 	requires javafx.swing;
 	requires static lombok;
 
-	opens com.alphagen.studio.FloatDataVisualizer to javafx.controls, javafx.fxml;
-	opens com.alphagen.studio.FloatDataVisualizer.buoyui.connections to javafx.controls, javafx.fxml;
-//    opens com.alphagen.studio.FloatDataVisualizer.datarecorder to javafx.fxml;
-//    opens com.alphagen.studio.FloatDataVisualizer.datareciever to com.fazecast.jSerialComm;
+	opens com.alphagen.studio.FloatDataVisualizer to javafx.fxml, javafx.controls;
+	opens com.alphagen.studio.FloatDataVisualizer.buoyui to javafx.fxml, javafx.controls;
+	opens com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.pages.connections to javafx.fxml, javafx.controls;
+	opens com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.pages.connections.editor to javafx.fxml, javafx.controls;
+	opens com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.pages.connections.datacard to javafx.fxml, javafx.controls;
+	opens com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.pages.connections.testing to javafx.fxml, javafx.controls;
+	opens com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.managers to javafx.fxml, javafx.controls;
+	opens com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.util to javafx.fxml, javafx.controls;
+	opens com.alphagen.studio.FloatDataVisualizer.buoyui.lib.fxml to javafx.fxml, javafx.controls;
 
 	exports com.alphagen.studio.FloatDataVisualizer;
-	exports com.alphagen.studio.FloatDataVisualizer.buoyui.connections;
-	exports com.alphagen.studio.FloatDataVisualizer.buoyui.backend.lib.jfilesystem;
-	exports com.alphagen.studio.FloatDataVisualizer.buoyui.backend.lib.jfilesystem.file;
-	exports com.alphagen.studio.FloatDataVisualizer.buoyui.backend.lib.jfilesystem.folder;
-	exports com.alphagen.studio.FloatDataVisualizer.buoyui.backend.lib.jfilesystem.file.exception;
-	exports com.alphagen.studio.FloatDataVisualizer.buoyui.backend;
-	exports com.alphagen.studio.FloatDataVisualizer.buoyui.backend.app;
-	//    exports com.alphagen.studio.FloatDataVisualizer.data;
-//    exports com.alphagen.studio.FloatDataVisualizer.data.util;
+	exports com.alphagen.studio.FloatDataVisualizer.buoyui;
+	exports com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.pages.connections;
+	exports com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.managers;
+	exports com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.util;
+	exports com.alphagen.studio.FloatDataVisualizer.buoyui.lib.fxml;
+	exports com.alphagen.studio.FloatDataVisualizer.buoyui.backend.data;
+	exports com.alphagen.studio.FloatDataVisualizer.buoyui.backend.util;
+	exports com.alphagen.studio.FloatDataVisualizer.buoyui.backend.constants;
+	exports com.alphagen.studio.FloatDataVisualizer.buoyui.backend.processor;
+	exports com.alphagen.studio.FloatDataVisualizer.buoyui.app;
+	exports com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.pages;
+	opens com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.pages to javafx.controls, javafx.fxml;
+
+
+//    opens com.alphagen.studio.FloatDataVisualizer.datareciever to com.fazecast.jSerialComm;
+
 //    exports com.alphagen.studio.FloatDataVisualizer.datareciever;
 //    exports com.alphagen.studio.FloatDataVisualizer.datarecorder;
 //    exports com.alphagen.studio.FloatDataVisualizer.datawriter;
-//    exports com.alphagen.studio.FloatDataVisualizer.filepaths;
-//    exports com.alphagen.studio.FloatDataVisualizer.log;
 }
