e222ad49d04db2bf61f1e159fb61f8805c978015
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-10 19:15:53 +0000

    merged to single: Linux

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/app/Platform.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/app/Platform.java
@@ -3,7 +3,5 @@ package com.alphagen.studio.FloatDataVisualizer.buoyui.backend.app;
 public enum Platform {
     WIN11, // 100%
     MACOS, // Apple Dev Account Needed
-    DEBIAN_LINUX,
-    REDHAT_LINUX,
-    NULL, LINUX // Device Needed
+    LINUX // Device Needed
 }
