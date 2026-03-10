02550b6db914363745a347843e0551d5296a35ef
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-10 12:00:47 -0700

    updated os detector

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/app/PlatformDetector.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/app/PlatformDetector.java
@@ -2,24 +2,30 @@ package com.alphagen.studio.FloatDataVisualizer.buoyui.backend.app;
 
 import lombok.Getter;
 
+import java.nio.file.Path;
+
 public class PlatformDetector {
-    @Getter
-    private static Platform OSPLATFORM;
+	@Getter
+	private static Platform OSPLATFORM;
+	private static Path PROJECTROOT;
 
-    public PlatformDetector() {
+	public PlatformDetector() {
+		detectPlatform();
+		setRootFolder();
+	}
 
-        String osName = System.getProperty("os.name");
-        if (osName.toLowerCase().contains("win"))
-            OSPLATFORM = Platform.WIN11;
-        else if (osName.toLowerCase().contains("mac") || osName.toLowerCase().contains("darwin")) {
-            OSPLATFORM = Platform.MACOS;
-        } else if (osName.toLowerCase().contains("linux")) {
-            OSPLATFORM = Platform.LINUX;
-        } else {
-            OSPLATFORM = Platform.NULL;
-        }
-    }
+	private void setRootFolder() {}
 
-    public void detectPlatform() {
-    }
+	public void detectPlatform() {
+		String osName = System.getProperty("os.name");
+		if (osName.toLowerCase().contains("win"))
+			OSPLATFORM = Platform.WIN11;
+		else if (osName.toLowerCase().contains("mac") || osName.toLowerCase().contains("darwin")) {
+			OSPLATFORM = Platform.MACOS;
+		} else if (osName.toLowerCase().contains("linux")) {
+			OSPLATFORM = Platform.LINUX;
+		} else {
+			OSPLATFORM = Platform.NULL;
+		}
+	}
 }
