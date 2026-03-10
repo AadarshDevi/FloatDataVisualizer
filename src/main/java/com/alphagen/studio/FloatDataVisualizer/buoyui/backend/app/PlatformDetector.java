a896059af45decca66875c6d2d3f4f7571843d78
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-10 19:16:29 +0000

    added linux path creation

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/app/PlatformDetector.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/app/PlatformDetector.java
@@ -2,30 +2,33 @@ package com.alphagen.studio.FloatDataVisualizer.buoyui.backend.app;
 
 import lombok.Getter;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class PlatformDetector {
 	@Getter
 	private static Platform OSPLATFORM;
+	@Getter
 	private static Path PROJECTROOT;
 
-	public PlatformDetector() {
+	public PlatformDetector() throws IOException {
 		detectPlatform();
-		setRootFolder();
 	}
 
 	private void setRootFolder() {}
 
-	public void detectPlatform() {
+	public void detectPlatform() throws IOException {
 		String osName = System.getProperty("os.name");
-		if (osName.toLowerCase().contains("win"))
+		if (osName.toLowerCase().contains("win")) {
 			OSPLATFORM = Platform.WIN11;
-		else if (osName.toLowerCase().contains("mac") || osName.toLowerCase().contains("darwin")) {
+			PROJECTROOT = Paths.get(System.getenv("APPDATA")).resolve("FloatDataVisualizer").resolve(AppData.RELEASE_VERSION);
+		} else if (osName.toLowerCase().contains("mac") || osName.toLowerCase().contains("darwin")) {
 			OSPLATFORM = Platform.MACOS;
 		} else if (osName.toLowerCase().contains("linux")) {
 			OSPLATFORM = Platform.LINUX;
-		} else {
-			OSPLATFORM = Platform.NULL;
+			PROJECTROOT = Paths.get(System.getProperty("user.home"), "FloatDataVisualizer/2.0.0/");
 		}
 	}
 }
