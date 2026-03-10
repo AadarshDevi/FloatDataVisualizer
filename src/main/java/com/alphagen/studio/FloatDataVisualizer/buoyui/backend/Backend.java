b9bc74f7284dc4a30f6f7400d5bca9e1a82e0d14
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-10 06:55:49 -0700

    rewrote file related code from custom library to a very few methods

---
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/Backend.java
@@ -0,0 +1,59 @@
+package com.alphagen.studio.FloatDataVisualizer.buoyui.backend;
+
+import com.alphagen.studio.FloatDataVisualizer.buoyui.app.AppData;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class Backend {
+
+	private static final Path PROJECTROOT = Paths.get(System.getenv("APPDATA")).resolve("FloatDataVisualizer").resolve(AppData.RELEASE_VERSION);
+
+	public static Backend backend;
+
+	private Backend() throws IOException {
+		Files.createDirectories(PROJECTROOT);
+	}
+
+	public static Backend getBackend() throws IOException {
+		if (backend == null) backend = new Backend();
+		return backend;
+	}
+
+	public boolean verifyFolder(String folder) throws IOException {
+		if (!Files.isDirectory(PROJECTROOT.resolve(folder)))
+			Files.createDirectories(PROJECTROOT.resolve(folder));
+		return true;
+	}
+
+	public boolean verifyFile(String file) throws IOException {
+		if (!Files.isRegularFile(PROJECTROOT.resolve(file)))
+			Files.createFile(PROJECTROOT.resolve(file));
+		return true;
+	}
+
+	public boolean verifyRoot() {
+		return Files.isDirectory(PROJECTROOT);
+	}
+
+//	public ArrayList<ConnectionConfig> getConnections() {
+//		try {
+//			Folder connectionsfolder = rootSystem.getFolder(FolderConstants.CONNECTIONS);
+//			System.out.println("Getting Connections Folder: " + connectionsfolder.getPath());
+//
+//			String[] files = connectionsfolder.list();
+//			if (files == null || files.length == 0) return null;
+//			System.out.println("Folder Content Count: " + files.length);
+//
+//			ArrayList<ConnectionConfig> connections = ConnectionProcessor.readAll(files);
+//
+//		} catch (FolderNotFoundException e) {
+//			System.out.println("Connections Folder not found");
+//		}
+//
+//		return null;
+//	}
+}
