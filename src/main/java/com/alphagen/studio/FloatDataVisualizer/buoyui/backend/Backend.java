9e7213407d80b435602385288b52ffb90a9bc7b6
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-10 18:01:18 -0700

    removed unused commented imports

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/Backend.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/Backend.java
@@ -1,46 +1,43 @@
 package com.alphagen.studio.FloatDataVisualizer.buoyui.backend;
 
-import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.app.AppData;
 import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.app.PlatformDetector;
 import lombok.Getter;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-
 
 public class Backend {
 
-    @Getter private static Path PROJECTROOT;
+	@Getter private static Path PROJECTROOT;
 
-    public static Backend backend;
+	public static Backend backend;
 
-    private Backend() throws IOException {
-        PROJECTROOT = PlatformDetector.getPROJECTROOT();
-        Files.createDirectories(PROJECTROOT);
-    }
+	private Backend() throws IOException {
+		PROJECTROOT = PlatformDetector.getPROJECTROOT();
+		Files.createDirectories(PROJECTROOT);
+	}
 
-    public static Backend getBackend() throws IOException {
-        if (backend == null) backend = new Backend();
-        return backend;
-    }
+	public static Backend getBackend() throws IOException {
+		if (backend == null) backend = new Backend();
+		return backend;
+	}
 
-    public boolean verifyFolder(String folder) throws IOException {
-        if (!Files.isDirectory(PROJECTROOT.resolve(folder)))
-            Files.createDirectories(PROJECTROOT.resolve(folder));
-        return true;
-    }
+	public boolean verifyFolder(String folder) throws IOException {
+		if (!Files.isDirectory(PROJECTROOT.resolve(folder)))
+			Files.createDirectories(PROJECTROOT.resolve(folder));
+		return true;
+	}
 
-    public boolean verifyFile(String file) throws IOException {
-        if (!Files.isRegularFile(PROJECTROOT.resolve(file)))
-            Files.createFile(PROJECTROOT.resolve(file));
-        return true;
-    }
+	public boolean verifyFile(String file) throws IOException {
+		if (!Files.isRegularFile(PROJECTROOT.resolve(file)))
+			Files.createFile(PROJECTROOT.resolve(file));
+		return true;
+	}
 
-    public boolean verifyRoot() {
-        return Files.isDirectory(PROJECTROOT);
-    }
+	public boolean verifyRoot() {
+		return Files.isDirectory(PROJECTROOT);
+	}
 
 //	public ArrayList<ConnectionConfig> getConnections() {
 //		try {
