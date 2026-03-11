5001376d722339acae559414e1c89ce46af978e6
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-11 11:00:25 -0700

    added root folder for each platform. not it is hopefully ready.

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/app/PlatformDetector.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/app/PlatformDetector.java
@@ -3,30 +3,30 @@ package com.alphagen.studio.FloatDataVisualizer.buoyui.backend.app;
 import lombok.Getter;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class PlatformDetector {
-	@Getter
-	private static Platform OSPLATFORM;
-	@Getter
-	private static Path PROJECTROOT;
+    @Getter
+    private static Platform OSPLATFORM;
+    @Getter
+    private static Path PROJECTROOT;
 
-	public PlatformDetector() throws IOException {
-		detectPlatform();
-	}
+    public PlatformDetector() throws IOException {
+        detectPlatform();
+    }
 
-	public void detectPlatform() throws IOException {
-		String osName = System.getProperty("os.name");
-		if (osName.toLowerCase().contains("win")) {
-			OSPLATFORM = Platform.WIN11;
-			PROJECTROOT = Paths.get(System.getenv("APPDATA")).resolve("FloatDataVisualizer").resolve(AppData.RELEASE_VERSION);
-		} else if (osName.toLowerCase().contains("mac") || osName.toLowerCase().contains("darwin")) {
-			OSPLATFORM = Platform.MACOS;
-		} else if (osName.toLowerCase().contains("linux")) {
-			OSPLATFORM = Platform.LINUX;
-			PROJECTROOT = Paths.get(System.getProperty("user.home"), "FloatDataVisualizer/2.0.0/");
-		}
-	}
+    public void detectPlatform() throws IOException {
+        String osName = System.getProperty("os.name");
+        if (osName.toLowerCase().contains("win")) {
+            OSPLATFORM = Platform.WIN11;
+            PROJECTROOT = Paths.get(System.getenv("APPDATA")).resolve("FloatDataVisualizer").resolve(AppData.RELEASE_VERSION);
+        } else if (osName.toLowerCase().contains("mac") || osName.toLowerCase().contains("darwin")) {
+            OSPLATFORM = Platform.MACOS;
+            PROJECTROOT = Paths.get(System.getProperty("user.home")).resolve("FloatDataVisualizer").resolve(AppData.RELEASE_VERSION);
+        } else if (osName.toLowerCase().contains("linux")) {
+            OSPLATFORM = Platform.LINUX;
+            PROJECTROOT = Paths.get(System.getProperty("user.home")).resolve("FloatDataVisualizer").resolve(AppData.RELEASE_VERSION);
+        }
+    }
 }
