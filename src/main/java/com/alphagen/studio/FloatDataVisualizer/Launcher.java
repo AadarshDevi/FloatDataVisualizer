7b712a3600b89a363d97ef51655491b8a23227e7
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-10 18:43:12 -0700

    stupid empty line auto removed

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/Launcher.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/Launcher.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 public class Launcher {
 
 	public static void main(String[] args) {
-
 		System.out.println("Starting FloatDataVisualizer");
 		Application.launch(BuoyUI.class, args);
 	}
