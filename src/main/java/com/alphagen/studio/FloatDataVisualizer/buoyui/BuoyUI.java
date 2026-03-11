51662cafdbceb7bdcf7fa5d5d6c19dc7bef9f37f
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-11 11:01:35 -0700

    detecting os removed from BuoyUI.java

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/BuoyUI.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/BuoyUI.java
@@ -20,67 +20,66 @@ import java.util.Objects;
 
 public class BuoyUI extends Application {
 
-	@Override
-	public void start(Stage stage) throws Exception {
+    @Override
+    public void start(Stage stage) throws Exception {
 
-		// fixme platform detection
-		PlatformDetector pd = new PlatformDetector();
-		pd.detectPlatform();
+        // fixme platform detection
+        PlatformDetector pd = new PlatformDetector();
 
-		System.out.println("Initializing Backend");
-		Backend backend = Backend.getBackend();
+        System.out.println("Initializing Backend");
+        Backend backend = Backend.getBackend();
 
-		System.out.println("Finding Folders");
+        System.out.println("Finding Folders");
 
-		Alert alert = new Alert(Alert.AlertType.ERROR);
-		alert.setHeaderText("Error");
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText("Error");
 
-		if (!backend.verifyRoot()) {
-			alert.setTitle("Exception 1");
-			alert.setContentText("Root folder is unable to be found or created.");
-			alert.showAndWait();
-			System.exit(-1);
-		}
+        if (!backend.verifyRoot()) {
+            alert.setTitle("Exception 1");
+            alert.setContentText("Root folder is unable to be found or created.");
+            alert.showAndWait();
+            System.exit(-1);
+        }
 
-		if (!backend.verifyFolder("connections")) {
-			alert.setTitle("Exception 2");
-			alert.setContentText("Base folder is unable to be found or created.\nBase Folder: connections");
-			alert.showAndWait();
-			System.exit(-1);
-		}
+        if (!backend.verifyFolder("connections")) {
+            alert.setTitle("Exception 2");
+            alert.setContentText("Base folder is unable to be found or created.\nBase Folder: connections");
+            alert.showAndWait();
+            System.exit(-1);
+        }
 
-		if (!backend.verifyFolder("logs")) {
-			alert.setTitle("Exception 3");
-			alert.setContentText("Base folder is unable to be found or created.\nBase Folder: logs");
-			alert.showAndWait();
-			System.exit(-1);
-		}
+        if (!backend.verifyFolder("logs")) {
+            alert.setTitle("Exception 3");
+            alert.setContentText("Base folder is unable to be found or created.\nBase Folder: logs");
+            alert.showAndWait();
+            System.exit(-1);
+        }
 
-		if (!backend.verifyFile("float.settings")) {
-			alert.setTitle("Exception 4");
-			alert.setContentText("File is unable to be found or created.\nFile: float.settings");
-			alert.showAndWait();
-			System.exit(-1);
-		}
-		System.out.println("All startup folders and settings file ready.");
+        if (!backend.verifyFile("float.settings")) {
+            alert.setTitle("Exception 4");
+            alert.setContentText("File is unable to be found or created.\nFile: float.settings");
+            alert.showAndWait();
+            System.exit(-1);
+        }
+        System.out.println("All startup folders and settings file ready.");
 
-		System.out.println("Loading ConnectionsUI");
+        System.out.println("Loading ConnectionsUI");
 
-		FXMLLoader fxmlLoader = new FXMLLoader(PageConstants.CONNECTIONS_PAGE);
-		BorderPane buoyUI = fxmlLoader.load();
-		ConnectionsController bmc = fxmlLoader.getController();
-		ControllerManager.setConnectionsController(bmc);
+        FXMLLoader fxmlLoader = new FXMLLoader(PageConstants.CONNECTIONS_PAGE);
+        BorderPane buoyUI = fxmlLoader.load();
+        ConnectionsController bmc = fxmlLoader.getController();
+        ControllerManager.setConnectionsController(bmc);
 
-		Scene scene = new Scene(buoyUI);
+        Scene scene = new Scene(buoyUI);
 
-		stage.setScene(scene);
-		stage.setTitle("Float Data Visualizer");
-		stage.getIcons().add(new Image(Objects.requireNonNull(Launcher.class.getResourceAsStream("buoyui/logos/ImageLogo.png"))));
-		StageManager.setMainStage(stage);
+        stage.setScene(scene);
+        stage.setTitle("Float Data Visualizer");
+        stage.getIcons().add(new Image(Objects.requireNonNull(Launcher.class.getResourceAsStream("buoyui/logos/ImageLogo.png"))));
+        StageManager.setMainStage(stage);
 
-		StageUtil.createInvisPane(stage, scene, buoyUI);
+        StageUtil.createInvisPane(stage, scene, buoyUI);
 
-		System.out.println("Opening App");
-		stage.show();
-	}
+        System.out.println("Opening App");
+        stage.show();
+    }
 }
