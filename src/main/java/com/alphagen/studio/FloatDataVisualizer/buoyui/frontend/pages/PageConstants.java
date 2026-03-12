3cf9eac00a172d51a1b29a8d9bcc680afc4b7b46
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-11 22:01:53 -0700

    updated to latest fxml: connections page and editor

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/pages/PageConstants.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/pages/PageConstants.java
@@ -4,7 +4,7 @@ import java.net.URL;
 
 public class PageConstants {
 	public static final String FRONTEND_ROOT = "/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend";
-	public static final URL CONNECTIONS_PAGE = PageConstants.class.getResource(FRONTEND_ROOT + "/pages/connections/Connections_v2.fxml");
-	public static final URL CONNECTIONS_EDITOR_PAGE = PageConstants.class.getResource(FRONTEND_ROOT + "/pages/connections/editor/ConnectionsEditor_v4.fxml");
+	public static final URL CONNECTIONS_PAGE = PageConstants.class.getResource(FRONTEND_ROOT + "/pages/connections/Connections_v3.fxml");
+	public static final URL CONNECTIONS_EDITOR_PAGE = PageConstants.class.getResource(FRONTEND_ROOT + "/pages/connections/editor/ConnectionsEditor_v5.fxml");
 	public static final URL LOADING_SCREEN = PageConstants.class.getResource(FRONTEND_ROOT + "/util/loading/ls_1.fxml");
 }
