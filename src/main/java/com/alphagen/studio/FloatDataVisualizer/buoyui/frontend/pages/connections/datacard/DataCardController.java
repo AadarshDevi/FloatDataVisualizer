0ccdd6a16bd3afc33fd658de17c5aa341e943711
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-12 15:38:51 -0700

    delete connection deletes the connection config ui and file

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/pages/connections/datacard/DataCardController.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/pages/connections/datacard/DataCardController.java
@@ -1,6 +1,7 @@
 package com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.pages.connections.datacard;
 
 import com.alphagen.studio.FloatDataVisualizer.buoyui.Controller;
+import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.constants.FolderConstants;
 import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.data.ConnectionConfig;
 import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.data.ConnectionType;
 import com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.managers.ControllerManager;
@@ -9,112 +10,114 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class DataCardController extends Controller {
-    @FXML public Button dataCard;
-    @FXML public Label baudRate;
-    @FXML public Label port;
-    @FXML public Label name;
-    @FXML public Button serial;
-    @FXML public Button wireless;
-    @FXML public ContextMenu connectionOptions;
-    @FXML public MenuItem deleteDataCard;
-    @Getter private ConnectionConfig connectionConfig;
+	@FXML public Button dataCard;
+	@FXML public Label baudRate;
+	@FXML public Label port;
+	@FXML public Label name;
+	@FXML public Button serial;
+	@FXML public Button wireless;
+	@FXML public ContextMenu connectionOptions;
+	@Getter private ConnectionConfig connectionConfig;
 
-    @Setter
-    @Getter
-    private boolean isDisabled;
+	@Setter
+	@Getter
+	private boolean isDisabled;
 
-    @Setter
-    @Getter
-    private boolean isWorking;
+	@Setter
+	@Getter
+	private boolean isWorking;
 
-    @FXML
-    public void initialize() {
-        if (dataCard.getContextMenu() == null) {
-            System.out.println("No ContextMenu Present");
-            dataCard.setContextMenu(connectionOptions);
-        }
-        System.out.println("Data 1-1: " + dataCard.hashCode());
-    }
+	@FXML
+	public void initialize() {
+		if (dataCard.getContextMenu() == null) {
+			System.out.println("No ContextMenu Present");
+			dataCard.setContextMenu(connectionOptions);
+		}
+	}
 
-    @FXML
-    public void handleContextMenu(MouseEvent event) {
-        if (!isDisabled && event.getButton() == MouseButton.SECONDARY)
-            connectionOptions.show(dataCard, event.getSceneX(), event.getSceneY());
-        else if (isDisabled) event.consume();
-    }
+	@FXML
+	public void handleContextMenu(MouseEvent event) {
+		if (!isDisabled && event.getButton() == MouseButton.SECONDARY)
+			connectionOptions.show(dataCard, event.getSceneX(), event.getSceneY());
+		else if (isDisabled) event.consume();
+	}
 
-    public void setConnection(String connectionName, int baudRateNum, SerialPort portName, ConnectionType connectionType) {
-        setConnectionName(connectionName);
-        setBaudRate(baudRateNum);
-        setPort(portName);
-        setConnectionType(connectionType);
-    }
+	public void setConnection(String connectionName, int baudRateNum, SerialPort portName, ConnectionType connectionType) {
+		setConnectionName(connectionName);
+		setBaudRate(baudRateNum);
+		setPort(portName);
+		setConnectionType(connectionType);
+	}
 
-    private void setConnectionName(String connectionName) {
-        System.out.println("Connection Start?");
-        System.out.println("--> " + connectionName);
-        invalidConnectionData();
-        name.setText(connectionName);
-    }
+	private void setConnectionName(String connectionName) {
+		invalidConnectionData();
+		name.setText(connectionName);
+	}
 
-    private void setBaudRate(int baudRateNum) {
-        baudRate.setText(Integer.toString(baudRateNum));
-    }
+	private void setBaudRate(int baudRateNum) {
+		baudRate.setText(Integer.toString(baudRateNum));
+	}
 
-    private void setPort(SerialPort portName) {
-        System.out.println("New Connection Set?");
-        System.out.println("--> " + portName);
-        invalidConnectionData();
-        port.setText(portName.getSystemPortName());
-    }
+	private void setPort(SerialPort portName) {
+		invalidConnectionData();
+		port.setText(portName.getSystemPortName());
+	}
 
-    private void setConnectionType(ConnectionType connectionType) {
-        System.out.println("Finding Connection");
-        switch (connectionType) {
-            case SERIAL:
-                serial.setVisible(true);
-                wireless.setVisible(false);
-                break;
-            case WIRELESS:
-                serial.setVisible(false);
-                wireless.setVisible(true);
-                break;
-            default:
-                System.out.println("Invalid Connection");
-                return;
-        }
-        System.out.println("Connection Type: " + connectionType);
-    }
+	private void setConnectionType(ConnectionType connectionType) {
+		switch (connectionType) {
+			case SERIAL:
+				serial.setVisible(true);
+				wireless.setVisible(false);
+				break;
+			case WIRELESS:
+				serial.setVisible(false);
+				wireless.setVisible(true);
+				break;
+			default:
+				System.out.println("Invalid Connection");
+		}
+	}
 
-    public void invalidConnectionData() {
-        System.out.println("Name: " + name);
-        System.out.println("Port: " + port);
-        if (name == null || port == null || port.getText().isEmpty() || name.getText().isEmpty()) {
-            isWorking = false;
-            isDisabled = true;
-            dataCard.setOpacity(0.4);
-        }
-    }
+	public void invalidConnectionData() {
+		if (name == null || port == null || port.getText().isEmpty() || name.getText().isEmpty()) {
+			isWorking = false;
+			isDisabled = true;
+			dataCard.setOpacity(0.4);
+		}
+	}
 
-    public void deleteConnection() {
-        System.out.println("Deleting Connection");
-        ControllerManager.getConnectionsController().deleteConnection(dataCard);
-    }
+	public void deleteConnection() {
+		System.out.println("Deleting Connection");
 
-    public void setConnection(ConnectionConfig connectionConfig) {
-        this.connectionConfig = connectionConfig;
-        System.out.println("Connection --> full config");
-        ControllerManager.getConnectionsController().setCurrentConnectionConfig(this.connectionConfig);
-        setBaudRate(connectionConfig.baudRate());
-        setConnectionName(connectionConfig.connectionName());
-        setPort(connectionConfig.port());
-        setConnectionType(connectionConfig.portType());
-    }
+		Path connectionFile = FolderConstants.CONNECTIONS.resolve(connectionConfig.connectionName() + FolderConstants.FLOAT_CONNECTION_FILE_EXTENSION);
+		try {
+			Files.delete(connectionFile);
+		} catch (IOException e) {
+			System.err.println("Unable to delete connection: " + connectionConfig.connectionName());
+			e.printStackTrace();
+			return;
+		}
+
+		ControllerManager.getConnectionsController().deleteConnection(dataCard);
+		System.out.println("Deleted Connection");
+	}
+
+	public void setConnection(ConnectionConfig connectionConfig) {
+		this.connectionConfig = connectionConfig;
+		ControllerManager.getConnectionsController().setCurrentConnectionConfig(this.connectionConfig);
+		setBaudRate(connectionConfig.baudRate());
+		setConnectionName(connectionConfig.connectionName());
+		setPort(connectionConfig.port());
+		setConnectionType(connectionConfig.portType());
+	}
 }
