fe7514dadbf206d6c2fe227ef9e94e0233401ca5
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-18 10:09:48 -0700

    disable data cards that have no connections available

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/pages/connections/ConnectionsController.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/pages/connections/ConnectionsController.java
@@ -128,4 +128,67 @@ public class ConnectionsController {
 		}
 		System.out.println("Added Connections: " + count);
 	}
+
+	@FXML
+	public void refreshConnections() {
+		ObservableList<Node> dataCards = connections.getChildren();
+		for (Node node : dataCards) {
+			Button dataCard = (Button) node;
+			DataCardController dcc = (DataCardController) dataCard.getProperties().get("dcc");
+
+			SerialPort sp = SerialPort.getCommPort(dcc.getConnectionConfig().port().getSystemPortName());
+			if (!sp.openPort()) {
+				dcc.disable();
+			} else {
+				dcc.enable();
+				sp.closePort();
+			}
+		}
+	}
+
+	@FXML
+	public void exportAllConnections() {
+		DirectoryChooser dc = new DirectoryChooser();
+		dc.setTitle("Export All Connections");
+		File folder = dc.showDialog(StageManager.getMainStage());
+		System.out.println(folder.getAbsolutePath());
+		Path exportRoot = Paths.get(folder.getAbsolutePath()).resolve("FloatDataVisualizer " + AppData.RELEASE_VERSION + " Connections");
+		System.out.println("Export Path: " + exportRoot);
+		try {
+			Files.createDirectories(exportRoot);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		File[] files = FolderConstants.CONNECTIONS.toFile().listFiles();
+		for (File file : files) {
+			try (
+					PrintWriter newFile = new PrintWriter(file);
+					BufferedReader oldFile = new BufferedReader(new FileReader(file))
+			) {
+//				if (file.exists()) {
+//					Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+//					alert.setTitle("Export Connection");
+//					alert.setHeaderText(null);
+//					alert.setContentText("File Exists. Do you want to overwrite it?");
+//					Optional<ButtonType> buttonType = alert.showAndWait();
+//					if (buttonType.isPresent() && buttonType.get().equals(ButtonType.OK)) {
+//						Files.delete(exportRoot.resolve(file.getName()));
+//						Files.copy(file.toPath(), exportRoot.resolve(file.getName()));
+//					}
+//				} else {
+//					Files.copy(file.toPath(), exportRoot.resolve(file.getName()));
+//				}
+//
+			} catch (IOException e) {
+//				System.err.println("Unable to copy file " + file.getAbsolutePath());
+//				e.printStackTrace();
+			}
+		}
+	}
+
+	@FXML
+	public void importConnection() {
+
+	}
 }
