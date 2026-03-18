e932ede926476fcd4e254d0fbc2a4431fda323a9
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-18 10:00:01 -0700

    connections now have the new data format

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/managers/ConnectionManager.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/managers/ConnectionManager.java
@@ -3,6 +3,7 @@ package com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.managers;
 import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.data.ConnectionConfig;
 import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.data.ConnectionType;
 import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.data.FloatConfig;
+import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.data.MeasurementConfig;
 import com.fazecast.jSerialComm.SerialPort;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,12 +18,12 @@ public class ConnectionManager {
 		currentConnection = null;
 	}
 
-	public ConnectionManager(String connectionName, int baudRate, SerialPort port, ConnectionType portType, FloatConfig floatConfig) {
-		createConnection(connectionName, baudRate, port, portType, floatConfig);
+	public ConnectionManager(String connectionName, int baudRate, SerialPort port, ConnectionType portType, FloatConfig floatConfig, MeasurementConfig[] measurementConfigs) {
+		createConnection(connectionName, baudRate, port, portType, floatConfig, measurementConfigs);
 	}
 
-	public void createConnection(String connectionName, int baudRate, SerialPort port, ConnectionType portType, FloatConfig floatConfig) {
-		currentConnection = new ConnectionConfig(connectionName, baudRate, port, portType, floatConfig);
+	public void createConnection(String connectionName, int baudRate, SerialPort port, ConnectionType portType, FloatConfig floatConfig, MeasurementConfig[] measurementConfigs) {
+		currentConnection = new ConnectionConfig(connectionName, baudRate, port, portType, floatConfig, measurementConfigs);
 	}
 
 	public ConnectionManager(ConnectionConfig connection) {
