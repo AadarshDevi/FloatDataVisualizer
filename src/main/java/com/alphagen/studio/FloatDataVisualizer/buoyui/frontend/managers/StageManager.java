a0bb3af3e27a860ebaeaba58fcebef21f34c3d05
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-21 16:44:41 -0700

    custom setter to add stage style

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/managers/StageManager.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/managers/StageManager.java
@@ -1,18 +1,28 @@
 package com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.managers;
 
+import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.util.DeltaDrag;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import lombok.Getter;
 import lombok.Setter;
 
 public class StageManager {
 
 	@Getter
-	@Setter
 	private static Stage mainStage;
 
 	@Getter
 	@Setter
 	private static Stage connectionCreatorStage;
+
+	public static void setMainStage(Stage stage) {
+		mainStage = stage;
+		mainStage.initStyle(StageStyle.TRANSPARENT);
+	}
+
 	public static void createInvisPane(Scene scene, Pane pane) {
 
 		scene.setFill(Color.TRANSPARENT);
