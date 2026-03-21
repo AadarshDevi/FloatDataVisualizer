36e262458c25d5eebc04f95c7be2bfd21690df25
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-21 12:45:52 -0700

    better data card creator and removed old data card creator.

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/managers/DataCardManager.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/managers/DataCardManager.java
@@ -1,17 +1,35 @@
 package com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.managers;
 
-import com.alphagen.studio.FloatDataVisualizer.buoyui.Controller;
 import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.data.ConnectionConfig;
 import com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.pages.CardConstants;
 import com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.pages.connections.datacard.DataCardController;
-import com.alphagen.studio.FloatDataVisualizer.buoyui.lib.fxml.NodePackage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 
 import java.io.IOException;
 
 public class DataCardManager {
-	public static <K extends Controller> NodePackage<K> create(ConnectionConfig connectionConfig) {
+//	public static <K extends Controller> NodePackage<K> create(ConnectionConfig connectionConfig) {
+//		FXMLLoader fxmlLoader = new FXMLLoader(CardConstants.CONNECTION_DATA_CARD);
+//		Button dataCard = null;  // Explicit type arg helps inference
+//		try {
+//			dataCard = fxmlLoader.load();
+//		} catch (IOException e) {
+//			throw new RuntimeException(e);
+//		}
+//		DataCardController dcc = fxmlLoader.getController();
+//		dcc.setConnection(connectionConfig);
+//
+//		dataCard.getProperties().put("dcc", dcc);
+//
+//		// Cast only if DataCardController isn't K-compatible
+//		return NodePackage.<K>builder()
+//				.node(dataCard)
+//				.controller((K) dcc)  // Safe if hierarchy correct
+//				.build();
+//	}
+
+	public static Button createDataCard(ConnectionConfig connectionConfig) {
 		FXMLLoader fxmlLoader = new FXMLLoader(CardConstants.CONNECTION_DATA_CARD);
 		Button dataCard = null;  // Explicit type arg helps inference
 		try {
@@ -23,12 +41,7 @@ public class DataCardManager {
 		dcc.setConnection(connectionConfig);
 
 		dataCard.getProperties().put("dcc", dcc);
-
-		// Cast only if DataCardController isn't K-compatible
-		return NodePackage.<K>builder()
-				.node(dataCard)
-				.controller((K) dcc)  // Safe if hierarchy correct
-				.build();
+		return dataCard;
 	}
 
 }
