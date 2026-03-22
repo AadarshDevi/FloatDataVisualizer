a9e6f46ff4cfcbd5fe5d7e3631c86fd49299fc40
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-21 18:34:18 -0700

    parses data

---
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/processor/DataProcessor.java
@@ -0,0 +1,16 @@
+package com.alphagen.studio.FloatDataVisualizer.buoyui.backend.processor;
+
+import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.data.DataPoint;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class DataProcessor implements Runnable {
+
+	LinkedBlockingQueue<DataPoint> parsedArray = new LinkedBlockingQueue<>();
+	LinkedBlockingQueue<DataPoint> rawArray = new LinkedBlockingQueue<>();
+
+	@Override
+	public void run() {
+
+	}
+}
