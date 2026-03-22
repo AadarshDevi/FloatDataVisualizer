3ccc28d9d346e237922d6f18343f1b3151636292
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-21 18:37:44 -0700

    removed unwanted annotation

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/data/DataPoint.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/data/DataPoint.java
@@ -1,9 +1,7 @@
 package com.alphagen.studio.FloatDataVisualizer.buoyui.backend.data;
 
 import lombok.Builder;
-import lombok.Data;
 
-@Data
 @Builder
 public record DataPoint(String teamInfo, int packetNum, double time, double[] measurements) {
 }
