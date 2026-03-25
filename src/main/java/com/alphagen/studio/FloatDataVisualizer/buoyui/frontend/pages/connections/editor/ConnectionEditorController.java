cda142b5787228e80d43f983d082a455c792f778
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-25 16:20:14 -0700

    made error labels 1 for each field. improved error handling by adding a lot of conditionals

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/pages/connections/editor/ConnectionEditorController.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/pages/connections/editor/ConnectionEditorController.java
@@ -23,15 +23,12 @@ import java.util.stream.Stream;
 
 public class ConnectionEditorController {
 
-	@FXML public Label error_label_connection_name_blank;
-	@FXML public Label error_label_name_invalid_characters;
-	@FXML public Label error_label_name_exists;
-	@FXML public Label error_label_baud_rate_invalid_characters;
-	@FXML public Label error_label_baud_rate_blank;
-	@FXML public Label error_label_port_blank;
-	@FXML public Label error_label_data_format_blank;
-	@FXML public Label error_label_start_flag_blank;
-	@FXML public Label error_label_end_flag_blank;
+	@FXML public Label error_label_connection_name;
+	@FXML public Label error_label_baud_rate;
+	@FXML public Label error_label_port;
+	@FXML public Label error_label_data_format;
+	@FXML public Label error_label_start_flag;
+	@FXML public Label error_label_end_flag;
 	@FXML TextField connectionName;
 	@FXML TextField baudRate;
 	@FXML ChoiceBox<String> connectionOptions;
@@ -64,39 +61,42 @@ public class ConnectionEditorController {
 	}
 
 	public void resetErrorLabels() {
-		error_label_connection_name_blank.setVisible(false);
-		error_label_baud_rate_invalid_characters.setVisible(false);
-		error_label_baud_rate_blank.setVisible(false);
-		error_label_port_blank.setVisible(false);
-		error_label_data_format_blank.setVisible(false);
+		error_label_connection_name.setText("");
+		error_label_baud_rate.setText("");
+		error_label_port.setText("");
+		error_label_data_format.setText("");
 
-		error_label_name_invalid_characters.setVisible(false);
-		error_label_name_exists.setVisible(false);
-		error_label_start_flag_blank.setVisible(false);
-		error_label_end_flag_blank.setVisible(false);
+		error_label_start_flag.setText("");
+		error_label_end_flag.setText("");
 	}
 
+	// todo: update error labels to have 1 label for each field
 	@FXML
 	public void confirmConnectionEditor() {
+
+		// todo: remove before release
+		startFlagTextField.setText("--start-data");
+		endFlagTextField.setText("--end-data");
 
 		boolean isValidConnectionName = isValidConnectionName();
 		boolean isValidBaudRate = validBaudRate();
 		boolean isValidPort = validPort();
 
+		// must always be before isValidMeasurementData() because isValidTeamName()
+		// does null, empty and invalid args checks
 		boolean isValidTeamName = isValidTeamName();
+		boolean isValidMeasurementData = false;
+		if (isValidTeamName)
+			isValidMeasurementData = isValidMeasurementData();
+
 		boolean isValidStartFlag = validStartFlag();
 		boolean isValidEndFlag = validEndFlag();
-
-		// todo: advanced booleans for checking if:
-		//  	1. team name and num exists (checking for "-" symbol)
-		//  	2. packet num exists (string "pkt-" exists)
-		//  	3. first thing after pkt- is time
-		//  	4. there is at least 1 measurement after time
 
 		System.out.println();
 		if (
 				isValidConnectionName && isValidBaudRate && isValidPort
-						&& isValidTeamName && isValidStartFlag && isValidEndFlag
+						&& isValidTeamName && isValidMeasurementData
+						&& isValidStartFlag && isValidEndFlag
 		) {
 			ConnectionConfig connection = new ConnectionConfig(
 					connectionName.getText(),
@@ -119,64 +119,59 @@ public class ConnectionEditorController {
 
 	private boolean isValidConnectionName() {
 		String value = connectionName.getText().trim();
-		if (value.isEmpty()) {
-			error_label_connection_name_blank.setVisible(true);
+		System.out.println("Connection Name: \"" + value + "\", \"" + connectionName.getText() + "\"");
+
+		if (value.isBlank()) {
+			error_label_connection_name.setText(ExceptionMessage.CONNECTION_NAME_EMPTY);
 			connectionName.getStyleClass().add(DynamicCSS.ERROR);
 			return false;
-		} else {
-			error_label_connection_name_blank.setVisible(false);
-			connectionName.getStyleClass().remove(DynamicCSS.ERROR);
 		}
 
 		for (char c : value.toCharArray()) {
 			if (!Character.isLetterOrDigit(c) && c != '-' && c != '_' && c != ' ') {
-				error_label_name_invalid_characters.setVisible(true);
+				error_label_connection_name.setText(ExceptionMessage.CONNECTION_NAME_INVALID_CHARACTERS);
 				connectionName.getStyleClass().add(DynamicCSS.ERROR);
 				return false;
 			}
 		}
 
-		error_label_name_invalid_characters.setVisible(false);
-		connectionName.getStyleClass().remove(DynamicCSS.ERROR);
-
 		File file = new File(FolderConstants.CONNECTIONS.resolve(value + FolderConstants.FLOAT_CONNECTION_FILE_EXTENSION).toString());
 		if (file.exists()) {
-			error_label_name_exists.setVisible(true);
+			error_label_connection_name.setText(ExceptionMessage.CONNECTION_NAME_EXISTS);
 			connectionName.getStyleClass().add(DynamicCSS.ERROR);
 			return false;
 		}
 
-		error_label_name_exists.setVisible(false);
+		error_label_connection_name.setText("");
 		connectionName.getStyleClass().remove(DynamicCSS.ERROR);
 		return true;
 	}
 
 	public boolean validBaudRate() {
-		if (baudRate.getText().trim().isEmpty()) {
-			error_label_baud_rate_blank.setVisible(true);
+		if (baudRate.getText().trim().isBlank()) {
+			error_label_baud_rate.setText(ExceptionMessage.CONNECTION_BAUD_RATE_EMPTY);
 			baudRate.getStyleClass().add(DynamicCSS.ERROR);
 			return false;
 		}
 
-		error_label_baud_rate_blank.setVisible(false);
-		baudRate.getStyleClass().remove(DynamicCSS.ERROR);
-
-		int baudRateValue = -1;
+		int baudRateValue;
 		try {
-			baudRateValue = Integer.parseInt(baudRate.getText());
-			error_label_baud_rate_invalid_characters.setVisible(false);
+			baudRateValue = Integer.parseInt(baudRate.getText().trim());
+			error_label_baud_rate.setText("");
 			baudRate.getStyleClass().remove(DynamicCSS.ERROR);
 		} catch (NumberFormatException e) {
-			error_label_baud_rate_invalid_characters.setVisible(true);
+			error_label_baud_rate.setText(ExceptionMessage.CONNECTION_BAUD_RATE_INVALID_CHARACTERS);
 			baudRate.getStyleClass().add(DynamicCSS.ERROR);
 			return false;
 		}
 
 		if (baudRateValue < 0) {
+			error_label_baud_rate.setText(ExceptionMessage.CONNECTION_BAUD_RATE_INVALID_RANGE);
 			baudRate.getStyleClass().add(DynamicCSS.ERROR);
 			return false;
 		}
 
+		error_label_baud_rate.setText("");
 		baudRate.getStyleClass().remove(DynamicCSS.ERROR);
 		return true;
 	}
@@ -192,77 +187,162 @@ public class ConnectionEditorController {
 			return false;
 		}
 
-		if (value == null || value.isEmpty()) {
-			error_label_port_blank.setVisible(true);
+		if (value == null || value.isBlank()) {
+			error_label_port.setText(ExceptionMessage.CONNECTION_PORT_EMPTY);
 			connectionOptions.getStyleClass().add(DynamicCSS.ERROR);
 			return false;
-		} else {
-			error_label_port_blank.setVisible(false);
-			connectionOptions.getStyleClass().remove(DynamicCSS.ERROR);
-			return true;
 		}
 
+		error_label_port.setText("");
+		connectionOptions.getStyleClass().remove(DynamicCSS.ERROR);
+		return true;
 	}
 
 	private boolean isValidTeamName() {
-		if (dataFormat.getText().trim().isEmpty()) {
-			error_label_data_format_blank.setVisible(true);
+		String floatFormat = dataFormat.getText().trim();
+		System.out.println("Float Data: \"" + floatFormat + "\", \"" + dataFormat.getText() + "\"");
+
+		// no text
+		if (floatFormat.isBlank()) {
+			System.err.println(" FD 1 >>> Empty");
+			error_label_data_format.setText(ExceptionMessage.FLOAT_DATA_FORMAT_EMPTY_ARGUMENTS);
+			dataFormat.getStyleClass().add(DynamicCSS.ERROR);
+			return false;
+		}
+		System.out.println(" FD 1 >>> Not Empty");
+
+		String[] floatFormatParts = floatFormat.split(",");
+
+		// team name/num dne
+		if (!floatFormatParts[0].contains("-")) {
+			System.err.println(" FD 3 >>> Invalid Team Info");
+			error_label_data_format.setText(ExceptionMessage.FLOAT_DATA_FORMAT_TEAM_DATA_ARGUMENTS_NOT_FOUND);
+			dataFormat.getStyleClass().add(DynamicCSS.ERROR);
+			return false;
+		}
+		System.out.println(" FD 3 >>> Valid Team Info");
+
+		// team info length = 2 (PN12, MiramarWaterJets)
+		String[] teamInfo = floatFormatParts[0].split("-");
+		System.out.println(" FD 4 >>> Team Number Length: " + teamInfo[0].length());
+
+		if (teamInfo.length != 2 && (teamInfo[0].isBlank() && teamInfo[1].isBlank())) {
+			System.err.println(" FD 5 >>> Invalid Data Length: length < 4");
+			error_label_data_format.setText(ExceptionMessage.FLOAT_DATA_FORMAT_TEAM_DATA_ARGUMENTS_MISSING + " or " + ExceptionMessage.FLOAT_DATA_FORMAT_TEAM_DATA_NAME_INVALID);
+			dataFormat.getStyleClass().add(DynamicCSS.ERROR);
+			return false;
+		}
+		System.out.println(" FD 5 >>> Valid Team Info");
+
+		// team num wrong (correct length = 4/5) ex: PN12, EX92
+		if (teamInfo[0].length() != 4) {
+			System.err.println(" FD 6 >>> Invalid Team Number");
+			error_label_data_format.setText(ExceptionMessage.FLOAT_DATA_FORMAT_TEAM_DATA_NUM_INVALID_LENGTH);
+			dataFormat.getStyleClass().add(DynamicCSS.ERROR);
+			return false;
+		}
+		System.out.println(" FD 6 >>> Valid Team Number");
+
+		// team num wrong (correct length = 4/5) ex: PN12, EX92
+		if (teamInfo.length < 2 || teamInfo[1].trim().isBlank()) {
+			System.err.println(" FD 6.2 >>> Invalid Team Number");
+			error_label_data_format.setText(ExceptionMessage.FLOAT_DATA_FORMAT_TEAM_DATA_NUM_INVALID_LENGTH);
+			dataFormat.getStyleClass().add(DynamicCSS.ERROR);
+			return false;
+		}
+		System.out.println(" FD 6.2 >>> Valid Team Number");
+
+		if (floatFormatParts.length < 2 || !floatFormatParts[1].contains("pkt-")) {
+			System.err.println(" FD 7 >>> Invalid Packet Format or Enter \"pkt-\"");
+			error_label_data_format.setText(ExceptionMessage.FLOAT_DATA_FORMAT_PACKET_ID_NOT_FOUND);
+			dataFormat.getStyleClass().add(DynamicCSS.ERROR);
+			return false;
+		}
+		System.out.println(" FD 7 >>> Valid Packet Format");
+
+		error_label_data_format.setText("");
+		dataFormat.getStyleClass().remove(DynamicCSS.ERROR);
+		return true;
+	}
+
+	private boolean isValidMeasurementData() {
+		String[] floatFormatParts = dataFormat.getText().trim().split(",");
+
+		try {
+			// first arg: contains time aka index 2 (0 - team info) (1 - pkt- info)
+			if (!floatFormatParts[2].trim().toLowerCase().contains("time")) {
+				error_label_data_format.setText(ExceptionMessage.MEASUREMENT_DATA_FORMAT_TIME_NOT_FOUND);
+				dataFormat.getStyleClass().add(DynamicCSS.ERROR);
+				return false;
+			}
+		} catch (ArrayIndexOutOfBoundsException _) {
+			error_label_data_format.setText(ExceptionMessage.MEASUREMENT_DATA_FORMAT_TIME_NOT_FOUND);
 			dataFormat.getStyleClass().add(DynamicCSS.ERROR);
 			return false;
 		}
 
-		error_label_data_format_blank.setVisible(false);
+		for (int i = 2; i < floatFormatParts.length; i++) {
+
+			String value = floatFormatParts[i];
+			if (!value.contains("(") || !value.contains(")")) {
+				error_label_data_format.setText(ExceptionMessage.MEASUREMENT_DATA_FORMAT_MEASUREMENT_UNIT_NOT_FOUND + value + "(unit)");
+				dataFormat.getStyleClass().add(DynamicCSS.ERROR);
+				return false;
+			}
+
+			if (!isValidMeasurementData(value.split("\\("))) {
+				error_label_data_format.setText(ExceptionMessage.MEASUREMENT_DATA_FORMAT_TIME_INVALID + ": " + value);
+				dataFormat.getStyleClass().add(DynamicCSS.ERROR);
+				return false;
+			}
+		}
+
+		// not enough args
+		if (floatFormatParts.length < 4) {
+			System.err.println(" FD 2 >>> Invalid Data Length: length < 4");
+			error_label_data_format.setText(ExceptionMessage.FLOAT_DATA_FORMAT_MISSING_ARGUMENTS);
+			dataFormat.getStyleClass().add(DynamicCSS.ERROR);
+			return false;
+		}
+		System.out.println(" FD 2 >>> Data Length" + floatFormatParts.length);
+
+		error_label_data_format.setText("");
 		dataFormat.getStyleClass().remove(DynamicCSS.ERROR);
 		return true;
 	}
 
 	private boolean validStartFlag() {
-		if (startFlagTextField.getText().trim().isEmpty()) {
-			error_label_start_flag_blank.setVisible(true);
+		if (startFlagTextField.getText().trim().isBlank()) {
+			error_label_start_flag.setText(ExceptionMessage.PROCESSOR_DATA_START_DATA_TRANSFER_FLAG_EMPTY);
 			startFlagTextField.getStyleClass().add(DynamicCSS.ERROR);
 			return false;
-		} else {
-			error_label_start_flag_blank.setVisible(false);
-			startFlagTextField.getStyleClass().remove(DynamicCSS.ERROR);
-			return true;
 		}
+
+		error_label_start_flag.setText("");
+		startFlagTextField.getStyleClass().remove(DynamicCSS.ERROR);
+		return true;
 	}
 
 	private boolean validEndFlag() {
-		if (endFlagTextField.getText().trim().isEmpty()) {
-			error_label_end_flag_blank.setVisible(true);
+		String value = endFlagTextField.getText().trim();
+		if (value.isBlank()) {
+			error_label_end_flag.setText(ExceptionMessage.PROCESSOR_DATA_END_DATA_TRANSFER_FLAG_EMPTY);
 			endFlagTextField.getStyleClass().add(DynamicCSS.ERROR);
 			return false;
-		} else {
-			error_label_end_flag_blank.setVisible(false);
-			endFlagTextField.getStyleClass().remove(DynamicCSS.ERROR);
-			return true;
 		}
-	}
 
-//	private boolean validMeasurementName() {
-//		if (measurementName.getText().trim().isEmpty()) {
-//			error_label_measure_name_blank.setVisible(true);
-//			measurementName.getStyleClass().add(DynamicCSS.ERROR);
-//			return false;
-//		} else {
-//			error_label_measure_name_blank.setVisible(false);
-//			measurementName.getStyleClass().remove(DynamicCSS.ERROR);
-//			return true;
-//		}
-//	}
-//
-//	private boolean validMeasurementUnit() {
-//		if (measurementUnit.getText().trim().isEmpty()) {
-//			error_label_measure_unit_blank.setVisible(true);
-//			measurementUnit.getStyleClass().add(DynamicCSS.ERROR);
-//			return false;
-//		} else {
-//			error_label_measure_unit_blank.setVisible(false);
-//			measurementUnit.getStyleClass().remove(DynamicCSS.ERROR);
-//			return true;
-//		}
-//	}
+
+		if (value.equals(startFlagTextField.getText().trim())) {
+			error_label_end_flag.setText(ExceptionMessage.PROCESSOR_DATA_TRANSFER_FLAG_SAME);
+			endFlagTextField.getStyleClass().add(DynamicCSS.ERROR);
+			return false;
+		}
+
+
+		error_label_end_flag.setText("");
+		endFlagTextField.getStyleClass().remove(DynamicCSS.ERROR);
+		return true;
+	}
 
 	private MeasurementConfig[] getMeasurementConfigs() {
 		String[] floatData = dataFormat.getText().trim().split(",");
@@ -279,6 +359,43 @@ public class ConnectionEditorController {
 		}
 		System.out.println();
 		return measurementConfigs;
+	}
+
+	private boolean isValidMeasurementData(String[] measurementData) {
+//		String[] measureData = null;
+//		try {
+//			measurementData[2].split("\\(");
+//		} catch (ArrayIndexOutOfBoundsException _) {
+//			error_label_data_format.setText(ExceptionMessage.MEASUREMENT_DATA_FORMAT_TIME_INVALID);
+//			dataFormat.getStyleClass().add(DynamicCSS.ERROR);
+//			return false;
+//		}
+
+		if (measurementData.length != 2) {
+			error_label_data_format.setText(ExceptionMessage.MEASUREMENT_DATA_FORMAT_TIME_INVALID);
+			dataFormat.getStyleClass().add(DynamicCSS.ERROR);
+			return false;
+		}
+
+		// measure name is empty
+		if (measurementData[0] == null || measurementData[0].isBlank()) {
+			error_label_data_format.setText(ExceptionMessage.MEASUREMENT_DATA_FORMAT_MEASUREMENT_NAME_NOT_FOUND);
+			dataFormat.getStyleClass().add(DynamicCSS.ERROR);
+			return false;
+		}
+
+		// measure unit is empty
+		// 		1. m) is min length (2)
+		//		2. ) is empty length (1)
+		if (measurementData[1].length() < 2) {
+			error_label_data_format.setText(ExceptionMessage.MEASUREMENT_DATA_FORMAT_MEASUREMENT_UNIT_NOT_FOUND);
+			dataFormat.getStyleClass().add(DynamicCSS.ERROR);
+			return false;
+		}
+
+		error_label_data_format.setText("");
+		dataFormat.getStyleClass().remove(DynamicCSS.ERROR);
+		return true;
 	}
 
 	@FXML
