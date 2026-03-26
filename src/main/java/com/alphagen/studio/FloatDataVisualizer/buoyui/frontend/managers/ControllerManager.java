474de4e540be81977afa6194f158bd45fa90dacb
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-03-25 19:41:59 -0700

    getter/setter for connection editor controller

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/managers/ControllerManager.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/managers/ControllerManager.java
@@ -1,12 +1,17 @@
 package com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.managers;
 
 import com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.pages.connections.ConnectionsController;
+import com.alphagen.studio.FloatDataVisualizer.buoyui.frontend.pages.connections.editor.ConnectionEditorController;
 import lombok.Getter;
 import lombok.Setter;
 
 public class ControllerManager {
 
-	@Setter
 	@Getter
+	@Setter
 	private static ConnectionsController connectionsController;
+
+	@Getter
+	@Setter
+	private static ConnectionEditorController connectionEditorController;
 }
