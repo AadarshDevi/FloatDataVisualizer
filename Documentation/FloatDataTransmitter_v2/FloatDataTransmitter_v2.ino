9fcd39e26ac467a19756270f2ce8d5fdfbc786e9
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-05-26 11:57:23 -0700

    fixed a few performance issue

--- Documentation/FloatDataTransmitter_v2/FloatDataTransmitter_v2.ino
+++ Documentation/FloatDataTransmitter_v2/FloatDataTransmitter_v2.ino
@@ -1,34 +1,41 @@
 
-const unsigned int baudRate = 115200;
-const String startDataFlag = "--start-data-transfer"
-const String endDataFlag = "--end-data-transfer"
+const unsigned int baudRate = 9600;
+const String startDataFlag = "--start-data-transfer";
+const String endDataFlag = "--end-data-transfer";
 
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(baudRate);
   randomSeed(analogRead(A0));
-  Serial.println("--start-data-transfer");
+  Serial.println(startDataFlag);
 }
 
 int i = 0;
 int packet_id = 0;
 
 void loop() {
-  unsigned long time_in_us = micros(); // us
-  float time_in_s = ((float)time_in_us) / 1000000; // s
+  float time_in_s = micros() / 1000000.0; // s
   long depth = random(0, 500);
   long pressure = random(100, 1000);
   if(i >= 50) {
-    delay(1000);
-    Serial.println("--end-data-transfer");
+    Serial.println(endDataFlag);
+    Serial.flush();
     Serial.end();
-    delay(1000);
-    return;
+    while (true) {
+      delay(1000);
+    }
   }
   if(i%20 == 0) {
     packet_id++;
   }
-  Serial.println("PN12-MiramarWaterJets,pkt-"+String(packet_id)+","+String(time_in_s)+","+String(depth)+","+String(pressure));
+  Serial.print("PN12-MiramarWaterJets,pkt-");
+  Serial.print(packet_id);
+  Serial.print(",");
+  Serial.print(time_in_s);
+  Serial.print(",");
+  Serial.print(depth);
+  Serial.print(",");
+  Serial.println(pressure);
   i++;
   delay(500);
 }
