fbbad5684ee735bf0785b5b48ea2b7a15598cecd
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-05-29 14:35:13 -0700

    moved themes to correct package

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/app/Theme.java
+++
@@ -1,6 +0,0 @@
-package com.alphagen.studio.FloatDataVisualizer.buoyui.backend.app;
-
-public enum Theme {
-	LIGHT,
-	DARK
-}
---
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/app/theme/Theme.java
@@ -0,0 +1,6 @@
+package com.alphagen.studio.FloatDataVisualizer.buoyui.backend.app.theme;
+
+public enum Theme {
+	LIGHT,
+	DARK
+}
