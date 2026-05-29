5eb1cf2466c794f2115e7cdca09975aae6ad7db6
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-05-29 15:41:11 -0700

    float.settings can now read and write theme

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/settings/SettingsManager.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/settings/SettingsManager.java
@@ -1,17 +1,18 @@
 package com.alphagen.studio.FloatDataVisualizer.buoyui.backend.settings;
 
 import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.app.AppMode;
+import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.app.theme.Theme;
+import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.app.theme.ThemeProcessor;
 import com.alphagen.studio.FloatDataVisualizer.buoyui.backend.constants.FolderConstants;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Properties;
 
 public class SettingsManager {
 
 	private static SettingsManager sm;
 	private final SettingsData settingsData;
+	private Properties properties;
 
 	private SettingsManager() {
 		settingsData = readSettings();
@@ -20,13 +21,15 @@ public class SettingsManager {
 	// todo: read settings
 	private SettingsData readSettings() {
 
-		final Properties properties = new Properties();
+		properties = new Properties();
 		try {
 			properties.load(new FileInputStream(new File(FolderConstants.SETTINGS.toUri())));
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 
+		ThemeProcessor.setTheme(Theme.valueOf(properties.getProperty("theme")));
+		System.out.println("theme: " + ThemeProcessor.getTheme());
 		return null;
 	}
 
@@ -46,7 +49,11 @@ public class SettingsManager {
 
 	// todo: write settings
 	public void writeSettings() {
-
+		try (PrintWriter pw = new PrintWriter(new File(FolderConstants.SETTINGS.toUri()))) {
+			pw.println("theme=" + ThemeProcessor.getTheme());
+		} catch (FileNotFoundException e) {
+			System.err.println("Unable to find settings file.");
+		}
 	}
 
 }
