947cd197ee03d2f809e7e5c92a7bc76f408be24d
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-05-29 16:26:46 -0700

    settings fields for easier placement in settings package

---
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/settings/SettingsField.java
@@ -0,0 +1,8 @@
+package com.alphagen.studio.FloatDataVisualizer.buoyui.backend.settings;
+
+public class SettingsField {
+	public static final String terminalAutoscroll = "autoscrollTerminal";
+	public static final String tableAutoScroll = "autoscrollTable";
+	public static final String theme = "theme";
+
+}
