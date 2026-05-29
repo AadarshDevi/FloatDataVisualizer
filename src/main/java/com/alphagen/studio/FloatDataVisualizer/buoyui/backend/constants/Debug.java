70acc54bfef818a432a92e7ae2b5b0ebc59f0265
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-05-29 10:47:23 -0700

    created a debugger which can enable and disable features

---
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/constants/Debug.java
@@ -0,0 +1,18 @@
+package com.alphagen.studio.FloatDataVisualizer.buoyui.backend.constants;
+
+public class Debug {
+	/**
+	 * To use window modes, set true
+	 */
+	public static boolean useWindowModes = false;
+
+	/**
+	 * To use theme changing, set true
+	 */
+	public static boolean useThemeChange = false;
+
+	/**
+	 * To use ___, set true
+	 */
+	public static boolean use = false;
+}
