e865aac8c25d3880de24c0f78602bdd9a0da1daf
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-05-29 08:12:10 -0700

    fixed a wierd bug. had to delete .idea folder

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,7 @@ module com.alphagen.studio.FloatDataVisualizer {
 	requires static lombok;
 	requires jdk.security.jgss;
 	requires java.sql;
+	requires org.apache.logging.log4j;
 	requires org.apache.logging.log4j.core;
 
 	opens com.alphagen.studio.FloatDataVisualizer to javafx.fxml, javafx.controls;
