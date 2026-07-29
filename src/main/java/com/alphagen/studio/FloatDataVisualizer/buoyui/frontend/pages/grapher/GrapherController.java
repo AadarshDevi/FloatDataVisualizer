fe8b2025a2c3d3c1cb37d16f4a3d37153138d7f4
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-29 09:30:23 -0700

    imports

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/pages/grapher/GrapherController.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/frontend/pages/grapher/GrapherController.java
@@ -41,6 +41,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class GrapherController {
     private final ExecutorService receiver = Executors.newSingleThreadExecutor();
