820df1e49e3555d57088f60afc933cddf74668e1
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-29 10:31:49 -0700

    issues with space unicode

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/processor/DataPointProcessor.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/processor/DataPointProcessor.java
@@ -13,54 +13,56 @@ public class DataPointProcessor implements Runnable {
     @Getter private final LinkedBlockingQueue<String> rawArray = new LinkedBlockingQueue<>();
     private final LinkedBlockingQueue<DataPoint> storedArray = new LinkedBlockingQueue<>();
 
-	@Override
-	public void run() {
+    @Override
+    public void run() {
 
-		while (!Thread.currentThread().isInterrupted()) {
-			String raw;
-			try {
-				raw = rawArray.take();
-			} catch (InterruptedException e) {
-				throw new RuntimeException(e);
-			}
+        System.out.println("[DataProcess] Ready");
 
-			try {
-//				System.out.println(" >>> " + raw);
+        while (!Thread.currentThread().isInterrupted()) {
+            String raw;
+            try {
+                raw = rawArray.take();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
                 System.out.println(" >>> " + raw);
+
                 if (!raw.startsWith(Connections.getCurrentConnection().floatConfig().teamData())) {
                     parsedArray.put(raw);
                     continue;
                 }
 
-				String[] parsed = raw.split(",");
+                String[] parsed = raw.split(",");
 
-				String[] array;
-				if (parsed.length > 4) {
-					array = Arrays.copyOfRange(parsed, 3, parsed.length);
-				} else {
-					array = new String[]{
-						parsed[3]
-					};
-				}
+                String[] array;
+                if (parsed.length > 4) {
+                    array = Arrays.copyOfRange(parsed, 3, parsed.length);
+                } else {
+                    array = new String[]{
+                            parsed[3]
+                    };
+                }
 
-				DataPoint dp = DataPoint.builder()
-					.teamInfo(parsed[0])
-					.packetNum(Integer.parseInt(parsed[1].replaceAll(
-						Connections.getCurrentConnection()
-							.floatConfig().pkt(),
-						""
-					)))
-					.time(Double.parseDouble(parsed[2].trim()))
-					.measurements(Arrays.stream(array)
-						.mapToDouble(Double::parseDouble)
-						.toArray())
-					.build();
+                DataPoint dp = DataPoint.builder()
+                        .teamInfo(parsed[0])
+                        .packetNum(Integer.parseInt(parsed[1].replaceAll(
+                                Connections.getCurrentConnection()
+                                        .floatConfig().pkt(),
+                                ""
+                        )))
+                        .time(Double.parseDouble(parsed[2].trim()))
+                        .measurements(Arrays.stream(array)
+                                .mapToDouble(Double::parseDouble)
+                                .toArray())
+                        .build();
 
-				parsedArray.add(dp);
+                parsedArray.add(dp);
 
-			} catch (Exception e) {
-				System.err.println(Arrays.toString(e.getStackTrace()));
-			}
-		}
-	}
+            } catch (Exception e) {
+                System.err.println(Arrays.toString(e.getStackTrace()));
+            }
+        }
+    }
 }
