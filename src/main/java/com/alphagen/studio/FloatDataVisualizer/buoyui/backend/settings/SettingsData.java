0dd37db2335b65e764439a24a3e0aabf0c3d2a1a
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-29 09:38:23 -0700

    added terminal verbose var and accessor and mod methods

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/settings/SettingsData.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/settings/SettingsData.java
@@ -7,23 +7,12 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 @Data
 class SettingsData {
-	// file data
-	private final String settingsVersion = "2.0.0";
-	// autoscroll
-	private final AtomicBoolean terminalAutoscroll = new AtomicBoolean(true);
-	private final AtomicBoolean tableAutoscroll = new AtomicBoolean(true);
-	// app data
-	private AppMode appMode = AppMode.USER;
-	// default autofill data
-	private String default_teamInfo = "";
-	private int default_baudRate = 115200;
-	private String default_startDataTransferFlag = "--start-data-transfer";
-	private String default_endDataTransferFlag = "--end-data-transfer";
     // file data
     private final String settingsVersion = "2.0.0";
     // autoscroll
     private final AtomicBoolean terminalAutoscroll = new AtomicBoolean(true);
     private final AtomicBoolean terminalVerbose = new AtomicBoolean(false);
+    private final AtomicBoolean tableAutoscroll = new AtomicBoolean(true);
     // app data
     private AppMode appMode = AppMode.USER;
     // default autofill data
@@ -47,5 +36,14 @@ class SettingsData {
     public void setAutoscrollTable(boolean autoscroll) {
         tableAutoscroll.set(autoscroll);
     }
+
+    public boolean getVerboseTerminal() {
+        return terminalVerbose.get();
+    }
+
+    public void setVerboseTerminal(boolean autoscroll) {
+        terminalVerbose.set(autoscroll);
+    }
+
 
 }
