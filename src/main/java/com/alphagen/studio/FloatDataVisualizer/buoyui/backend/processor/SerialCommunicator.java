125358122d67fc6e66b33bce2b73975f93887bad
Aadarsh D <aadarsh123456char@gmail.com>
2026-08-14 11:56:09 -0700

    comment removed

--- src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/processor/SerialCommunicator.java
+++ src/main/java/com/alphagen/studio/FloatDataVisualizer/buoyui/backend/processor/SerialCommunicator.java
@@ -102,7 +102,6 @@ public class SerialCommunicator implements Runnable {
             running.set(true);
 
             System.out.println("[SerialReader] Ready");
-//            System.out.println("[SerialReader] 3. Ready - " + bufferedReader.ready());
 
             // 2. read stream
             while (running.get() && !Thread.currentThread().isInterrupted()) {
